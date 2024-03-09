62640a3b1186914a684c731fa19424fe00e3aee8
ratatuli3 <86473315+ratatuli3@users.noreply.github.com>
2024-03-09 16:00:14 +0100

    Update organize.py

--- organize.py
+++ organize.py
@@ -8,7 +8,7 @@ def monitorDownloads(directory):
         for fileName in Files:
             dotIndex = fileName.rfind('.')
             filePath = os.path.join(directory, fileName)
-            fileDirectory = os.path.join("C:\\Destination\\Directory\\Path\\Here", (fileName[dotIndex+1:]).upper())
+            fileDirectory = os.path.join("C:\\Destination\\Directory\\Path\\Here", (fileName[dotIndex+1:]).upper()) #This is the directory that you are going to put the directories for the files (example Videos/EXE/fortnite.exe)
             fileDestination = os.path.join(fileDirectory, fileName)
             os.makedirs(fileDirectory, exist_ok=True)
             os.rename(filePath, fileDestination)
@@ -16,5 +16,5 @@ def monitorDownloads(directory):
 
 
 if __name__ == "__main__":
-    downloadsDirectory = "C:\\Full\\Path\\Here"
+    downloadsDirectory = "C:\\Full\\Path\\Here" #This is the folder that will be checked, change as needed.
     monitorDownloads(downloadsDirectory)
